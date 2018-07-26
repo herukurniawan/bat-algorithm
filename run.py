394688a79686a560aa5910ced0ac8bb71925b3e3
Balasubramanyam Evani <balasubramanyam.evani@gmail.com>
2018-07-26 07:23:22 +0530

    Added new testing function Michalewicz

--- run.py
+++ run.py
@@ -7,5 +7,19 @@ def fungsi(d):
         jum = jum + (d[i]*d[i])
     return jum
 
+### Added Michalewicz Function
+
+def Michalewicz(pos):
+    result = 0.0
+    for i in range(0 , len(pos)):
+        a = math.sin(pos[i])
+        b = math.sin(((i+1) * pos[i] * pos[i] ) / math.pi)
+        c = math.pow(b , 20)
+        result += a*c
+    return -1.0 * result
+
+
 ba = BatAlgorithm(2,1000,10000,0.1,0.95,0.95,0,1,-1,1,fungsi)
+#ba = BatAlgorithm(5,50,1000,0.10,0.95,0.95,0,1,0,np.pi,Michalewicz) ## Michalewicz
+
 ba.proses_ba()
